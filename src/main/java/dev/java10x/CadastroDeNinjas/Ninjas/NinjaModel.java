7131d2348b6dd4ab84ee61b36b2cf41396b37772
Matheus Barbato <matheus.leoonel@gmail.com>
2024-12-09 15:08:32 -0300

    fix: Adiciona o Column na classe

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -17,13 +17,20 @@ public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
+
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
 
+    @Column(name = "idade")
     private int idade;
+
+    @Column(name = "img_url")
+    private String imgUrl;
 
     @ManyToOne // Muitos ninjas podem ter uma missao
     @JoinColumn(name = "missoes_id")
