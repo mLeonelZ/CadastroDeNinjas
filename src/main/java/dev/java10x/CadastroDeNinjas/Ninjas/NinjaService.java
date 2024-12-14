2bb3c9de97b091fc185cd3d4fcdbc764d570a365
Matheus Barbato <matheus.leoonel@gmail.com>
2024-12-14 15:59:53 -0300

    feat: inclui a lógica para listar apenas os emails e fix: altera tipo long -> Long

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -13,7 +13,7 @@ public class NinjaService {
 
     //@Autowired // Inicializa os construtores
     private final NinjaRepository ninjaRepository; // conectar ao repositório // INJEÇAO DE DEPENDENCIA
-    NinjaMapper ninjaMapper = new NinjaMapper();
+    NinjaMapper ninjaMapper;
 
     // Boa pratica injetar com o construtor
     public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
@@ -30,8 +30,14 @@ public class NinjaService {
         return ninjas.stream().map(ninjaMapper::map).collect(Collectors.toList());
     }
 
+    // Lógica para listar apenas os Emails
+    public List<String> listarEmails(){
+        List<NinjaModel> ninjas = ninjaRepository.findAll();
+        return ninjas.stream().map(NinjaModel::getEmail).toList();  // com base na lista de ninjas, cria uma nova lista apenas com os emails
+    }
+
     // Lógica Buscar Ninja por ID
-    public NinjaDTO ninjaPorID(long id){
+    public NinjaDTO ninjaPorID(Long id){
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
         return ninjaPorId.map(ninjaMapper::map).orElse(null);
     }
@@ -43,12 +49,12 @@ public class NinjaService {
     }
 
     // Deletar Ninja TEM QUE SER VOID
-    public void deletarNinjaID(long id){
+    public void deletarNinjaID(Long id){
         ninjaRepository.deleteById(id);
     }
 
     // Atualizar Ninja
-    public NinjaDTO atualizarNinja(long id,NinjaDTO ninjaDTO){
+    public NinjaDTO atualizarNinja(Long id,NinjaDTO ninjaDTO){
         Optional<NinjaModel> ninjaExistente = ninjaRepository.findById(id);
         if(ninjaExistente.isPresent()) {
             NinjaModel ninjaAtualizado = ninjaMapper.map(ninjaDTO);
