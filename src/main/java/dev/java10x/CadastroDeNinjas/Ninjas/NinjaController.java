da4f7b91931a63a218b55da6a6379238c5430890
Matheus Barbato <matheus.leoonel@gmail.com>
2024-12-15 21:14:45 -0300

    refactor: adiciona final a injeção das dependências e também o thymeleaf para renderização

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 public class NinjaController {
 
     // Injetando a dependencia do service
-    private NinjaService ninjaService;
+    private final NinjaService ninjaService;
 
     public NinjaController(NinjaService ninjaService) {
         this.ninjaService = ninjaService;
